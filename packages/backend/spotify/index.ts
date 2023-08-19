749fdde5a5fd138bf4e4fe05817b0ac3a4fd3393
Behn <7383025+BehnH@users.noreply.github.com>
2023-08-19 08:31:44 +0100

    feat-be: Export spotify client as SpotifyClient

--- packages/backend/spotify/index.ts
+++ packages/backend/spotify/index.ts
@@ -1,3 +1,5 @@
 export * as SpotifyLoginApi from './auth/login';
 export * as SpotifyPlayerRecent from './player/recent';
 export * as Crypt from './auth/crypt';
+
+export * as SpotifyClient from './client';
