e41a908b5177c772d1a0b8e170595533a0bf9f65
Behn <7383025+BehnH@users.noreply.github.com>
2023-09-20 21:32:41 +0100

    be/analytics: Update analytics functions

--- backend/src/spotify/analytics/artists.ts
+++ backend/src/spotify/analytics/artists.ts
@@ -1,164 +1,78 @@
 
 import { prisma } from "../..";
-import { startOfToday, startOfWeek, endOfToday, endOfYesterday, startOfYesterday } from 'date-fns'
+import { endOfToday, endOfYesterday, startOfToday, startOfWeek, startOfYesterday } from 'date-fns';
 
-const getPastPeriodToCurrent = async (userId: string, period: "pastday" | "pastweek" | "all"): Promise<{ track: { artists: { id: string }[] } }[]> => {
-    const nowSeconds = Math.floor(Date.now() / 1000);
-    const pastDaySeconds = nowSeconds - 86400;
-    const pastWeekSeconds = nowSeconds - 604800;
-
-    let periodValue: number;
-
-    switch (period) {
-        case "pastday":
-            periodValue = Math.floor(new Date(startOfToday()).getTime() / 1000);
-            break;
-        case "pastweek":
-            periodValue = Math.floor(new Date(startOfWeek(startOfToday())).getTime() / 1000);
-            break;
-        case "all":
-            periodValue = 0;
-            break;
-    }
-
-    return await prisma.playHistory.findMany({
-        where: {
-            user: {
-                id: userId
-            },
-            playedAt: {
-                gte: periodValue
-            },
-        },
-        select: {
-            track: {
-                select: {
-                    artists: {
-                        select: {
-                            id: true
-                        }
-                    }
-                }
-            }
-        },
-    });
-};
-
-const getPastPeriodToPrevious = async (userId: string, period: "pastday" | "pastweek" | "all"): Promise<{ track: { artists: { id: string }[] } }[]> => {
-    const nowSeconds = Math.floor(Date.now() / 1000);
-    const pastDaySeconds = nowSeconds - 86400;
-    const pastDaySecondsToPreviousDay = nowSeconds - (86400 * 2);
-    const pastWeekSeconds = nowSeconds - 604800;
-    const pastWeekSecondsToPreviousWeek = nowSeconds - (604800 * 2);
-
-    let periodValue: number;
-    let previousPeriodValue: number;
-
-    switch (period) {
-        case "pastday":
-            periodValue = pastDaySeconds;
-            previousPeriodValue = pastDaySecondsToPreviousDay;
-            break;
-        case "pastweek":
-            periodValue = pastWeekSeconds;
-            previousPeriodValue = pastWeekSecondsToPreviousWeek;
-            break;
-        case "all":
-            periodValue = 0;
-            previousPeriodValue = 0;
-            break;
-    }
-
-    return await prisma.playHistory.findMany({
-        where: {
-            user: {
-                id: userId
-            },
-            playedAt: {
-                gte: periodValue,
-                lte: previousPeriodValue
-            },
-        },
-        select: {
-            track: {
-                select: {
-                    artists: {
-                        select: {
-                            id: true
-                        }
-                    }
-                }
-            }
-        },
-    });
-}
-
-const getUniqueArtistCount = (historyMap: { track: { artists: { id: string }[] } }[]) => historyMap.map((item) => item.track.artists.map((artist) => artist.id)).reduce((a, b) => a.concat(b), []).filter((item, index, array) => array.indexOf(item) === index).length;
-
-export const artistCount = async (userId: string, type: "pastday" | "pastweek" | "all"): Promise<object> => {
-    const nowSeconds = Math.floor(Date.now() / 1000);
-    const pastWeekSeconds = nowSeconds - 604800;
+export const uniqueArtistCount = async (userId: string, type: string) => {
+    let baseVals: { lte: number, gte: number };
+    let prevVals: { lte: number, gte: number };
 
     switch (type) {
         case "pastday":
-            const pastDay = await getPastPeriodToCurrent(userId, type);
-            const pastPreviousDay = await getPastPeriodToPrevious(userId, type);
+            baseVals = { lte: Math.floor(new Date(endOfToday()).getTime() / 1000), gte: Math.floor(new Date(startOfToday()).getTime() / 1000) };
+            prevVals = { lte: Math.floor(new Date(endOfYesterday()).getTime() / 1000), gte: Math.floor(new Date(startOfYesterday()).getTime() / 1000) };
+            break;
 
-            // Reduce the artists array, remove duplications, and return a single number
-            const pastDayUnique = getUniqueArtistCount(pastDay);
-            const pastPreviousDayUnique = getUniqueArtistCount(pastPreviousDay);
-
-            const pastDayResponse = {
-                count: pastDayUnique,
-                diffCount: pastDayUnique - pastPreviousDayUnique,
-                diffPercent: Math.floor(((pastDayUnique - pastPreviousDayUnique) / pastDay.length) * 100),
-            }
-
-            return pastDayResponse;
-        case "pastweek":
-            const pastWeek = await prisma.playHistory.count({
-                where: {
-                    user: {
-                        id: userId
-                    },
-                    playedAt: {
-                        gte: pastWeekSeconds
-                    }
-                }
-            });
-
-            const pastWeekDiff = pastWeek - await prisma.playHistory.count({
-                where: {
-                    user: {
-                        id: userId
-                    },
-                    playedAt: {
-                        gte: pastWeekSeconds - 604800,
-                        lte: pastWeekSeconds
-                    }
-                }
-            });
-
-            const pastWeekResponse = {
-                count: pastWeek,
-                diffCount: pastWeekDiff,
-                diffPercent: Math.floor((pastWeekDiff / pastWeek) * 100)
-            }
-
-            return pastWeekResponse;
-
-        case "all":
-            const allTime = await prisma.playHistory.count({
-                where: {
-                    user: {
-                        id: userId
-                    }
-                }
-            });
-
-            return {
-                count: allTime,
-                diff: 0
-            }
+        default:
+            baseVals = { lte: Math.floor(new Date(endOfToday()).getTime() / 1000), gte: Math.floor(new Date(startOfToday()).getTime() / 1000) };
+            prevVals = { lte: Math.floor(new Date(endOfYesterday()).getTime() / 1000), gte: Math.floor(new Date(startOfYesterday()).getTime() / 1000) };
+            break;
     }
-};
+
+    const basePeriodSearch = await prisma.playHistory.findMany({
+        where: {
+            user: {
+                id: userId
+            },
+            playedAt: {
+                ...baseVals
+            },
+        },
+        select: {
+            track: {
+                select: {
+                    artists: {
+                        select: {
+                            id: true
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    const prevPeriodSearch = await prisma.playHistory.findMany({
+        where: {
+            user: {
+                id: userId
+            },
+            playedAt: {
+                ...prevVals
+            }
+        },
+        select: {
+            track: {
+                select: {
+                    artists: {
+                        select: {
+                            id: true
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    const basePeriodUnique = getUniqueArtistCount(basePeriodSearch);
+    const prevPeriodUnique = getUniqueArtistCount(prevPeriodSearch);
+
+    const res = {
+        count: basePeriodUnique,
+        prevDayCount: prevPeriodUnique,
+        diffPercent: Math.floor((prevPeriodUnique / basePeriodUnique) * 100),
+        diffRaw: basePeriodUnique - prevPeriodUnique,
+    }
+
+    return res;
+}
+
+const getUniqueArtistCount = (historyMap: { track: { artists: { id: string }[] } }[]) => historyMap.map((item) => item.track.artists.map((artist) => artist.id)).reduce((a, b) => a.concat(b), []).filter((item, index, array) => array.indexOf(item) === index).length;
--- backend/src/spotify/analytics/tracks.ts
+++ backend/src/spotify/analytics/tracks.ts
@@ -102,3 +102,48 @@ export const trackCount = async (userId: string, type: "pastday" | "pastweek" | 
             }
     }
 };
+
+export const trackCountGraph = async (userId: string, type: "day" | "week" | "month" | "year"): Promise<object> => {
+    if (type === "day") return await getPastDayByHour(userId);
+
+    return {
+        success: false,
+        message: "Not implemented",
+        data: null
+    }
+};
+
+const getPastDayByHour = async (userId: string): Promise<object> => {
+    const today = Math.floor(new Date(startOfToday()).getTime() / 1000);
+
+    const pastDay = await prisma.playHistory.findMany({
+        where: {
+            user: {
+                id: userId
+            },
+            playedAt: {
+                gte: today,
+            }
+        },
+        select: {
+            playedAt: true
+        }
+    });
+
+    // Convert the timestamps to hours and then group in to an hour based array
+    const pastDayHours = pastDay.map((item) => {
+        return Math.floor((item.playedAt - today) / 3600);
+    });
+
+    // Create hour array
+    const hours = Array.from(Array(24).keys()).map((item) => new Object({ hour: item, count: 0 }));
+
+
+    // Map the pastDayHours array to the hours array
+    pastDayHours.forEach((item) => {
+        // @ts-ignore
+        hours[item].count++;
+    });
+
+    return hours;
+};
