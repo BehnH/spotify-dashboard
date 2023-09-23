bcb843fa4ccea6b34ca94135f3c47096eaa8f996
Behn <7383025+BehnH@users.noreply.github.com>
2023-09-23 10:23:26 +0100

    feat/api: Add artist routes

--- backend/src/routes/artist.ts
+++ backend/src/routes/artist.ts
@@ -1,4 +1,34 @@
 import { Router } from "express";
+import z from "zod";
+import { validateRequest } from "zod-express-middleware";
+import { prisma } from "..";
 
 const router = Router();
 export default router;
+
+router.get("/:id", validateRequest({
+    params: z.object({
+        id: z.string().nonempty()
+    })
+}), async (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+
+    const artist = await prisma.artist.findUnique({
+        where: {
+            id: id,
+        },
+        include: {
+            images: true,
+            tracks: {
+                include: {
+                    album: true,
+                }
+            }
+        }
+    });
+
+    if (!artist) return res.status(404).send("Artist not found");
+
+    return res.status(200).json(artist);
+});
