0f652e94859da3c1149c10abdafd513e8797d8d0
Behn <7383025+BehnH@users.noreply.github.com>
2023-10-13 19:50:31 +0100

    ui/style: Use default bg for login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function AuthPage() {
     return (
-        <div className="bg-gray-950">
+        <>
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-slate-900 border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -41,6 +41,6 @@ export default function AuthPage() {
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
